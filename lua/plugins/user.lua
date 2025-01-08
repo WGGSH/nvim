f120bec35f1b92afda0d1f2349d211051976f346
yasuda <yasuda@mfac.jp>
2025-01-08 13:00:15 +0900

    change color scheme

--- lua/plugins/user.lua
+++ lua/plugins/user.lua
@@ -417,7 +417,7 @@ return {
     config = function()
       require("catppuccin").setup {
         flavour = "macchiato",
-        -- transparent_background = true,
+        transparent_background = true,
         integrations = {
           barbar = false,
           illuminate = {
