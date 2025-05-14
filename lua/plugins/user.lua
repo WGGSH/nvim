1378b2c0eeac02c09629d38809318f413399d689
yasuda <yasuda@mfac.jp>
2025-05-14 15:15:59 +0900

    nvim.cmp

--- lua/plugins/user.lua
+++ lua/plugins/user.lua
@@ -822,6 +822,14 @@ return {
         },
         -- 必要なら他のマッピングもここで定義
       },
+      completion = {
+        -- Set to false to disable completion.
+        nvim_cmp = true,
+        -- Trigger completion at 2 chars.
+        min_chars = 2,
+      },
     },
-  }
+  },
+
+
 }
